7e22825b27507407bcb536aaa4226dc82533d8e7
Valmir Dornas <fernandocursos2021@gmail.com>
2025-08-29 20:51:33 -0300

    Tela Splash

--- app/src/main/java/com/valorizeseusamigos/topfilms/MainActivity.kt
+++ app/src/main/java/com/valorizeseusamigos/topfilms/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.valorizeseusamigos.topfilms.presenter.splash.SplashScreen
 import com.valorizeseusamigos.topfilms.ui.theme.TopFilmsTheme
 
 
@@ -20,31 +14,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TopFilmsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize(),
-                    containerColor = TopFilmsTheme.colorScheme.backgroundColor
-                ) { innerPadding ->
-                    Greeting(
-                        name = "Jesus",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SplashScreen()
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TopFilmsTheme {
-        Greeting("Jesus")
-    }
-}
